830337d4d114df9f6f878b3c166927276ebd76b4
OldWarrior3000 <xxxx@xxxx.com>
2017-10-18 08:12:54 -0700

    Initial Commit

---
+++ WebApiTest/Controllers/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiTest.Controllers
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Name { get; set; }
+        public int HorsePowers { get; set; }
+    }
+}
---
+++ WebApiTest/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiTest.Controllers
+{
+    public class TestController : ApiController
+    {
+        List<Car> cars = new List<Car>()
+        {
+            new Car() {  Brand = "Opel",Name="Astra", HorsePowers = 100 },
+            new Car() {  Brand = "Opel",Name="Antera", HorsePowers = 150 },
+            new Car() {  Brand = "VW",Name="Golf", HorsePowers = 120 }
+        };
+
+        public IEnumerable<Car> GetAllCars()
+        {
+            return cars;
+        }
+
+
+        public IHttpActionResult GetCar(string id)
+        {
+            if (string.IsNullOrEmpty(id)) throw new ArgumentNullException("Name must have a value");
+
+            var car = cars.Where(x => x.Name == id).FirstOrDefault();
+            if (car == null) return NotFound();
+
+            return Ok(car);
+        }
+    }
+}
+
